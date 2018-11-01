e502483d1ed3400a4ac2ad97a3f1b351f476409e
KMO <kai.mcgilligan.oliver@gmail.com>
2018-11-01 21:48:07 +0000

    Gitignore and first entity component

---
+++ Assets/Scripts/Entities/ComponentTypes.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public struct PlayerInput : IComponentData {
+
+    public float3 Move;
+}
+
+public struct Health : IComponentData
+{
+    public float Value;
+}
+
+public struct MoveSpeed : IComponentData
+{
+    public float speed;
+}
